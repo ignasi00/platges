ad63a27b25016f7143efe15b2cc0d3cbb952b5a8
Ignasi <ignasi@annamarco.com>
2021-11-04 12:47:33 +0100

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   datasets/__init__.py
    	new file:   datasets/platges_datasets.py
    	new file:   models/__init__.py
    	new file:   models/algorithms/__init__.py
    	new file:   models/algorithms/homography.py
    	new file:   platges.py

---
+++ platges.py
@@ -0,0 +1,43 @@
+
+import cv2
+from torch.utils.data import DataLoader
+
+from datasets.platges_datasets import Platges_DronHomographyDataset
+from models.algorithms.homography import BasicStitching, RANSAC_MatrixFinder
+
+
+DATA_PATH = '/mnt/c/Users/Ignasi/Downloads/4fotos_ret' #'/mnt/c/Users/Ignasi/Downloads/PlatgesBCN/dron'
+SAVE_PATH = '/mnt/c/Users/Ignasi/Downloads/panorama_test.jpg'
+
+
+if __name__ == "__main__":
+    
+    dataset = Platges_DronHomographyDataset(DATA_PATH, 
+                                            to_tensor=False, 
+                                            cluster=True,#True, 
+                                            th_time=60, 
+                                            longitude_bin_size=180, 
+                                            latitude_bin_size=180, 
+                                            min_per_bin=1,
+                                            downsample=4)#4)
+    def my_collate(x): return x # <- do not transform imgs to tensor
+    dataloader = DataLoader(dataset, collate_fn=my_collate)
+
+    # TODO: mask beach from cityscapes
+
+    bf = cv2.BFMatcher(crossCheck=True) # Brute Force Matcher
+    matrix_finder = RANSAC_MatrixFinder(point_matcher=bf,
+                                        MIN_MATCH_COUNT=4,
+                                        ransacReprojThreshold=10,
+                                        maxIters=1000)
+    sticher = BasicStitching(homography_matrix_estimator=matrix_finder)
+
+    for batch in dataloader:
+        for imgs, metas in batch:
+            print('\n'.join([f"{i} - {meta['path']}" for i, meta in enumerate(metas)]))
+            print("\n---\n")
+            panorama = sticher(imgs[0:], verbose=True, plot=False)
+
+            cv2.imwrite(SAVE_PATH, panorama)
+
+            raise
